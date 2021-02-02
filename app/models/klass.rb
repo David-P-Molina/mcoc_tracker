2799784c2439d24c311bc4e02cdc92a1e4a67c41
David Moina <davemolina777@gmail.com>
2021-02-02 14:49:54 -0600

    Added realtions between models

--- app/models/klass.rb
+++ app/models/klass.rb
@@ -1,2 +1,4 @@
 class KLASS < ActiveRecord::Base
+    has_many :champions
+    has_many :users, through: :champions
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class USER < ActiveRecord::Base
     has_secure_password #creates papssword password=, authenticate, password confirmation ##requires bcrypt ###password has to be present
     has_many :champions
-    
+    has_many :klass, through: :champions
 end
