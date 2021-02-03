c22a3641e74c4b7d76090b27f00c05e5a8bc57d6
David Moina <davemolina777@gmail.com>
2021-02-03 10:15:41 -0600

    Commented out unneccesary pages

--- app/controllers/champions_controller.rb
+++ app/controllers/champions_controller.rb
@@ -5,33 +5,33 @@ class ChampionsController < ApplicationController
     erb :"/champions/index.html"
   end
 
-  # GET: /champions/new
-  get "/champions/new" do
-    erb :"/champions/new.html"
-  end
+  # # GET: /champions/new
+  # get "/champions/new" do
+  #   erb :"/champions/new.html"
+  # end
 
-  # POST: /champions
-  post "/champions" do
-    redirect "/champions"
-  end
+  # # POST: /champions
+  # post "/champions" do
+  #   redirect "/champions"
+  # end
 
   # GET: /champions/5
   get "/champions/:id" do
     erb :"/champions/show.html"
   end
 
-  # GET: /champions/5/edit
-  get "/champions/:id/edit" do
-    erb :"/champio_ns/edit.html"
-  end
+  # # GET: /champions/5/edit
+  # get "/champions/:id/edit" do
+  #   erb :"/champio_ns/edit.html"
+  # end
 
-  # PATCH: /champions/5
-  patch "/champions/:id" do
-    redirect "/champions/:id"
-  end
+  # # PATCH: /champions/5
+  # patch "/champions/:id" do
+  #   redirect "/champions/:id"
+  # end
 
-  # DELETE: /champions/5/delete
-  delete "/champions/:id/delete" do
-    redirect "/champions"
-  end
+  # # DELETE: /champions/5/delete
+  # delete "/champions/:id/delete" do
+  #   redirect "/champions"
+  # end
 end
