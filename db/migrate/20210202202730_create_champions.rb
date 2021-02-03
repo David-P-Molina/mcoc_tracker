b89e44c9dc8c88722062412f0d0cfaad599c1fcb
David Moina <davemolina777@gmail.com>
2021-02-03 09:15:22 -0600

    added columns to champion table

--- db/migrate/20210202202730_create_champions.rb
+++ db/migrate/20210202202730_create_champions.rb
@@ -3,7 +3,14 @@ class CreateChampion < ActiveRecord::Migration[5.2]
     create_table :champions do |t|
       t.string :name
       t.string :klass
-
+      t.boolean :favorite
+      t.boolean :wanted
+      t.boolean :one_star
+      t.boolean :two_star
+      t.boolean :three_star
+      t.boolean :four_star
+      t.boolean :five_star
+      t.boolean :six_star
     end
   end
 end
