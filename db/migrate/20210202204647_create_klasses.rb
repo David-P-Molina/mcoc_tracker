b13c028997e17a40dfe3f47e1e01e220cae3c68c
David Moina <davemolina777@gmail.com>
2021-02-03 08:00:17 -0600

    added columns to all tables

--- db/migrate/20210202204647_create_klasses.rb
+++ db/migrate/20210202204647_create_klasses.rb
@@ -1,7 +1,9 @@
 class CreateKlass < ActiveRecord::Migration
   def change
     create_table :klasses do |t|
-
+      t.string :name
+      t.string :strong_against
+      t.string :weak_against
       t.timestamps null: false
     end
   end
