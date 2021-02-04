2fd55d27192d4d5f7bfc30a78db48b4fcda21ff7
David Moina <davemolina777@gmail.com>
2021-02-04 17:22:53 -0600

    added sessions, enabled session secret and added .env file

--- db/migrate/20210202202730_create_champion.rb
+++ db/migrate/20210202202730_create_champion.rb
@@ -3,14 +3,15 @@ class CreateChampion < ActiveRecord::Migration[5.2]
     create_table :champions do |t|
       t.string :name
       t.string :klass
-      t.boolean :favorite, default: false
-      t.boolean :wanted, default: false
       t.boolean :one_star, default: false#explain why 6 columns are necessary
       t.boolean :two_star, default: false
       t.boolean :three_star, default: false
       t.boolean :four_star, default: false
       t.boolean :five_star, default: false
       t.boolean :six_star, default: false
+      t.boolean :favorite, default: false
+      t.boolean :wanted, default: false
+      t.string :notes
     end
   end
 end
