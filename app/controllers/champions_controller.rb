47951c6e3ae1de015e92b543c150ae8438fbf5f9
David Moina <davemolina777@gmail.com>
2021-02-03 18:40:12 -0600

    removed routes not needed

--- app/controllers/champions_controller.rb
+++ app/controllers/champions_controller.rb
@@ -9,29 +9,4 @@ class ChampionsController < ApplicationController
     erb :"/champions/show.html"
   end
   
-  # # GET: /champions/new
-  # get "/champions/new" do
-  #   erb :"/champions/new.html"
-  # end
-
-  # # POST: /champions
-  # post "/champions" do
-  #   redirect "/champions"
-  # end
-
-
-  # # GET: /champions/5/edit
-  # get "/champions/:id/edit" do
-  #   erb :"/champio_ns/edit.html"
-  # end
-
-  # # PATCH: /champions/5
-  # patch "/champions/:id" do
-  #   redirect "/champions/:id"
-  # end
-
-  # # DELETE: /champions/5/delete
-  # delete "/champions/:id/delete" do
-  #   redirect "/champions"
-  # end
 end
--- app/controllers/klasses_controller.rb
+++ app/controllers/klasses_controller.rb
@@ -9,29 +9,4 @@ class KlassesController < ApplicationController
     get "/classes/:id" do
       erb :"/klasses/show.html"
     end
-
-  # # GET: /klasses/new
-  # get "/classes/new" do
-  #   erb :"/klasses/new.html"
-  # end
-
-  # # POST: /klasses
-  # post "/classes" do
-  #   redirect "/klasses"
-  # end
-
-  # # GET: /klasses/5/edit
-  # get "/classes/:id/edit" do
-  #   erb :"/klasses/edit.html"
-  # end
-
-  # # PATCH: /klasses/5
-  # patch "/classes/:id" do
-  #   redirect "/klasses/:id"
-  # end
-
-  # # DELETE: /klasses/5/delete
-  # delete "/classes/:id/delete" do
-  #   redirect "/klasses"
-  # end
 end
