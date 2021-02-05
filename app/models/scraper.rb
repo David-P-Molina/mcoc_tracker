a7f0e0a257b62efff81d247552473f2eb71838ea
David Moina <davemolina777@gmail.com>
2021-02-05 08:21:23 -0600

    Used DRY principle on scraper class

--- app/models/scraper.rb
+++ app/models/scraper.rb
@@ -1,31 +1,25 @@
 require 'nokogiri'
 require 'open-uri'
 require 'pry'
-#
+
+require_relative './champion.rb'
+
 class Scraper
     def get_page
-        #binding.pry
-        
+        doc = Nokogiri::HTML(open("https://marvel-contestofchampions.fandom.com/wiki/List_of_Champions"))
     end
     def get_champions
-        doc = Nokogiri::HTML(open("https://marvel-contestofchampions.fandom.com/wiki/List_of_Champions"))
-        rows = doc.css(".wikitable tbody tr")
-        rows.each do |row|
-           champion = row.css("td").text.strip.split("\n")
-           name = champion[0]
-           release_date = champion[1]
-           klass = champion[2]
-           new_champ = "Champion.create(#{name}, #{release_date}, #{klass})"
-           p new_champ
-        end
-       
+        self.get_page.css(".wikitable tbody tr")
     end
-    # def make_champions
-    #     self.get_champions.each do |champ|
-    #         champion = Champion.new
-    #         champion.name = 
-    #         champion.klass = 
-    #     end
-    # end
+    def make_champions
+        self.get_champions.each do |row|
+            binding.pry
+            champion = Champion.create
+            info = row.css("td").text.strip.split("\n")
+            champion.name = info[0]
+            champion.release_date = info[1]
+            champion.klass = info[2]
+        end
+    end
 end
-Scraper.new.get_champions
+Scraper.new.make_champions
