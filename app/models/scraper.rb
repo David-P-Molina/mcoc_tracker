9604ab708b2dfbd10d540e177fb089224557731c
David Moina <davemolina777@gmail.com>
2021-02-04 20:52:02 -0600

    Created scraper to grab the champion info needed

--- app/models/scraper.rb
+++ app/models/scraper.rb
@@ -11,9 +11,14 @@ class Scraper
         doc = Nokogiri::HTML(open("https://marvel-contestofchampions.fandom.com/wiki/List_of_Champions"))
         rows = doc.css(".wikitable tbody tr")
         rows.each do |row|
-            binding.pry
-            row.css("td").text
+           champion = row.css("td").text.strip.split("\n")
+           name = champion[0]
+           release_date = champion[1]
+           klass = champion[2]
+           new_champ = "Champion.create(#{name}, #{release_date}, #{klass})"
+           p new_champ
         end
+       
     end
     # def make_champions
     #     self.get_champions.each do |champ|
