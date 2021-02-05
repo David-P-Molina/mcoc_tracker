90f2e7b5bb7b8b9eef00345ff856c1fc5de9cd24
David Moina <davemolina777@gmail.com>
2021-02-05 13:53:46 -0600

    Added relationship info and added questions for meeting

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
     has_secure_password #creates papssword password=, authenticate, password confirmation ##requires bcrypt ###password has to be present
-    has_one :roster
+    has_one :roster, dependent: :destroy
     has_many :champions, through: :roster
     #has_many :klasses, through: :champions #am i using this?
     #Learn what this and regex specifics
