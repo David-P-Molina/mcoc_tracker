17898e11e2f03c345c3849536978995387d8299e
David Moina <davemolina777@gmail.com>
2021-02-05 15:33:47 -0600

    enabled sessions and added hex code to .env

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,8 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
-    # enable :sessions #review
-    # set :session_secret, ENV['SESSION_SECRET']
+    enable :sessions #review
+    set :session_secret, ENV['SESSION_SECRET']
   end
 
   get "/" do
