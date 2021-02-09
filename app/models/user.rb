21d7da8c03b4f6237a37fcd209d5cc24421417fa
David Moina <davemolina777@gmail.com>
2021-02-09 11:35:13 -0600

    added slug and find by slug methods to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,11 @@ class User < ActiveRecord::Base
     validates :password, length:{in: 8..30}, confirmation: true, on: :create
     validates :username, uniqueness: true, length: {in: 8..20}
     validates :email, presence: true, uniqueness: true, format: {with: /\A(?<username>[^@\s]+)@((?<domain_name>[a-z0-9]+)\.(?<domain>[a-z]{2,}))\z/i}
+    
+    def slug
+        username.downcase.gsub(" ", "-")
+    end
+    def self.find_by_slug(slug)
+        User.all.find{|user| user.slug == slug}
+    end
 end
