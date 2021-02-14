3cbb42b91593aa6c81dc13ba26e4bb7220f68e8f
David Moina <davemolina777@gmail.com>
2021-02-13 18:22:00 -0600

    Added images to champions, renamed images

--- app/controllers/champions_controller.rb
+++ app/controllers/champions_controller.rb
@@ -2,6 +2,7 @@ class ChampionsController < ApplicationController
 
   # GET: /champions ##Will show all champions, maybe a way to organize by name, klass, release date
   get "/champions" do
+    @champions = Champion.order(:name, :asc)
     erb :"/champions/index"
   end
   # # GET: /champions/5
