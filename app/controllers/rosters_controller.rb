96c8ee1e4165e83d9db8be5b0b6731bdf4e02ccc
David Moina <davemolina777@gmail.com>
2021-02-18 13:30:30 -0600

    changed names to honor restful convention

--- app/controllers/rosters_controller.rb
+++ app/controllers/rosters_controller.rb
@@ -1,21 +1,21 @@
 class RostersController < ApplicationController
 
   # GET: /rosters ##Will include the information for how to find and identify your roster
-  get "/roster/instructions" do
+  get "/rosters/instructions" do
     redirect_if_not_logged_in  
-    erb :"/roster/instructions"
+    erb :"/rosters/instructions"
   end
   
   
   # GET: /rosters/new ##Will contain the form that will have their roster information. Will probably break down by klass type
   #will have instructions on how to fill out the form
-  get "/roster/new" do
+  get "/rosters/new" do
     redirect_if_not_logged_in
     @champions = Champion.all.order(:name)
-    erb :"/roster/new"
+    erb :"/rosters/new"
   end
   # POST: /rosters
-  post "/roster" do
+  post "/rosters" do
     redirect_if_not_logged_in
     params[:roster][:champions].each do |hash|
       if hash[1].keys.length > 1 
@@ -35,49 +35,49 @@ class RostersController < ApplicationController
     end
     if current_user
       flash[:success] = "Successfully added roster to database."
-      redirect "/roster/:id"
+      redirect "/rosters/:id"
     else
       flash[:error] = "Unable to add roster to database, Please try again."
-      redirect "/roster/new"
+      redirect "/rosters/new"
     end
   end
   
   
   # GET: /rosters/5# shows the person their roster and allows them to make changes if need be
-  get "/roster/:id" do
+  get "/rosters/:id" do
     redirect_if_not_logged_in
     binding.pry
     @roster = Roster.find_by_id(params[:id])
-    erb :"/roster/show"
+    erb :"/rosters/show"
   end
   
   # GET: /rosters/5/edit
-  get "/roster/:id/edit" do
+  get "/rosters/:id/edit" do
     redirect_if_not_logged_in
     @roster = Roster.find_by_id(params[:id])
     if not_the_owner?(@roster)
-      redirect to "/roster/instructions"
+      redirect to "/rosters/instructions"
     else
       erb :"/roster/edit"
     end
   end
   
   # PATCH: /rosters/5
-  patch "/roster/:id" do
-    redirect "/roster/:id"
+  patch "/rosters/:id" do
+    redirect "/rosters/:id"
   end
   
   # DELETE: /rosters/5/delete
-  delete "/roster/:id/delete" do
+  delete "/rosters/:id/delete" do
     redirect_if_not_logged_in
       roster = Roster.find_by(params[:id])
       if not_the_owner?(@roster)
         flash[:error] = "You do not have permission to make changes to this file!"
-        redirect to "/roster/instructions"
+        redirect to "/rosters/instructions"
       else
         roster.delete
         flash[:success] = "Roster has been successfully deleted!"
-        redirect to "/roster/new"
+        redirect to "/rosters/new"
       end
   end
 end
