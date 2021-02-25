35ffb7a4427243ea013d9d6cdd5b17d13f2603e2
David Moina <davemolina777@gmail.com>
2021-02-25 08:35:31 -0600

    Removed comments and formatted code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,5 @@
 class UsersController < ApplicationController
 
-  # GET: /users/new
   get "/signup" do
     if !logged_in?
     erb :"/users/signup"
@@ -9,7 +8,6 @@ class UsersController < ApplicationController
     end
   end
 
-  # POST: /users
   post "/signup" do
     user = User.create(params['user'])
     if user.valid?
@@ -22,7 +20,7 @@ class UsersController < ApplicationController
     end
   end
 
-   # GET: /login
+
   get '/login' do
     if !logged_in?
       erb :"users/login"
@@ -33,7 +31,6 @@ class UsersController < ApplicationController
 
   post '/login' do
     user = User.find_by(email: params['user'][:email])
-    #binding.pry
     if user && user.authenticate(params['user'][:password])
       session[:user_id] = user.id
       flash[:success] = "Successfully Logged In!"
@@ -44,28 +41,10 @@ class UsersController < ApplicationController
     end
   end
   
+  
   get '/logout' do
       session.destroy
       redirect to '/login'
   end
-  
-  # # GET: /users/5
-  # get "/users/:id" do
-  #   erb :"/users/show"
-  # end
 
-  # # GET: /users/5/edit
-  # get "/users/:id/edit" do
-  #   erb :"/users/edit"
-  # end
-
-  # # PATCH: /users/5
-  # patch "/users/:id" do
-  #   redirect "/users/:id"
-  # end
-
-  # # DELETE: /users/5/delete
-  # delete "/users/:id/delete" do
-  #   redirect "/users"
-  # end
 end
